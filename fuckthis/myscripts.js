492530608ad94307f56765f7e18c3a76c55b4a56
Danial <osama.al-midani@outlook.com>
2021-03-17 11:32:17 +0100

    some comments

--- fuckthis/myscripts.js
+++ fuckthis/myscripts.js
@@ -1,18 +1,26 @@
-'use strict';
+'use strict';// why use a strict mode ??
+
 function openForm() {
+    //add Jquery to project to use (form.show() or form.hide())
     document.getElementById("myForm").style.display = "block";
 }
 
 function closeForm() {
+    //add Jquery to project to use (form.show() or form.hide())
     document.getElementById("myForm").style.display = "none";
 }
 
 function adding() {
-    var table = document..getElementById("youtubrs");
+    //less copy paste.. more learn and do 
+    //https://code-boxx.com/create-table-from-array-javascript/
+    var table = document.getElementById("youtubrs");
     var row = table.insertRow(0);
     var cell1 = row.insertCell(0);
     var cell2 = row.insertCell(1);
     cell1.innerHTML = "new cell1";
     cell2.innerHTML = "new cell2";
-    
+
 }
+
+
+//Save the data in the browser so it stays saved if we refreshed the page
